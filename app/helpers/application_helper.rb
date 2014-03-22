81eb864f6196668f121b96f0457406ffea0c61b3
gwholt <ahnbizcad@gmail.com>
2014-03-22 02:53:10 -0700

    customize Devise forms and alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	#def auto_select_text_field_tag(name, value = nil, options {})
+	#	text_field_tag(name,
+	#						 		 value,
+	#						 		 { :onclick => "this.select()",
+	#						 		   :readonly => "true" }.merge(options))	
+	#end
+
 end
